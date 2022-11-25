9417851dc806e1de9ead1296c6c7451718f38b5b
Максим Сивов <m.sivov@ideco.ru>
2022-11-25 18:14:18 +0600

    Created things_backend app

--- things/things/settings.py
+++ things/things/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'things_backend.apps.ThingsBackendConfig',
 ]
 
 MIDDLEWARE = [
---
+++ things/things_backend/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ThingsBackendConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'things_backend'
